9ba17e9d3d5b043cf7c6c1cd02ce47bf136ff50c
shivgany5 <160329038+shivgany5@users.noreply.github.com>
2025-08-29 13:39:49 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(file_path: str) -> List[str]:
     return requirements
 
 setup(
-    name = "mlproject",
+    name = "mlproject", 
     version = "0.0.1",
     author = "Yash Shivgan",
     author_email = "shivgany5@gmail.com",
