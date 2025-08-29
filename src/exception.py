3104eea6d914b9d4b83ff0551623416b718a043f
shivgany5 <160329038+shivgany5@users.noreply.github.com>
2025-08-29 21:43:42 +0530

    EDA & Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,13 @@
 import sys
-import logging 
+from logger import logging
+##from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
+        file_name, exc_tb.tb_lineno, str(error))
+    
     return error_message
 
 class CustomException(Exception):
@@ -18,3 +19,30 @@ class CustomException(Exception):
         return self.error_message
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if __name__ == "__main__":  
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by zero exception occurred")
+#         raise CustomException(e,sys)
